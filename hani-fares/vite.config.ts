5dfff32961a76885914c83a9ae8527f78370c103
hanifares <hani.fares93@gmail.com>
2025-08-03 21:25:26 +0200

    init commit

---
+++ hani-fares/vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  base: '/hani-fares/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+    port: 5001,
+  },
+})
