1b3f147335e4d3972bbbda883d27256c12c5cf5a
hanifares <hani.fares93@gmail.com>
2025-08-04 18:59:35 +0200

    Github workflow

--- hani-fares/vite.config.ts
+++ hani-fares/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/hani-fares/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
