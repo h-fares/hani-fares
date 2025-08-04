7f1759bf068fa3f463e8c705819b18ebccb4a45e
hanifares <hani.fares93@gmail.com>
2025-08-04 19:02:09 +0200

    Github workflow

--- hani-fares/src/App.vue
+++ hani-fares/src/App.vue
@@ -11,8 +11,8 @@
 </template>
 
 <script setup>
-import Header from '@/components/layout/Header.vue'
-import Footer from '@/components/layout/Footer.vue'
+import Header from '@/components/Layout/Header.vue'
+import Footer from '@/components/Layout/Footer.vue'
 </script>
 
 <style>
